f57429925c22d9199cb7e1067d54b42bf01ec84f
waynematt28-bot215 <waynematt28@gmail.com>
2026-07-30 12:07:53 -0400

    Delete chore-week-app directory

--- chore-week-app/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="chore-week-v1";
-const FILES=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
